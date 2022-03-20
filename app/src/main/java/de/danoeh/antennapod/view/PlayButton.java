55dcbeca5618bedd9381a072a0ba198bfb77b450
ErgoProxy1 <jaafar.kaoussarani@polymtl.ca>
2022-03-20 13:10:43 -0400

    anom 4 fix

--- app/src/main/java/de/danoeh/antennapod/view/PlayButton.java
+++ app/src/main/java/de/danoeh/antennapod/view/PlayButton.java
@@ -1,6 +1,7 @@
 package de.danoeh.antennapod.view;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -31,22 +32,30 @@ public class PlayButton extends AppCompatImageButton {
     public void setIsShowPlay(boolean showPlay) {
         if (this.isShowPlay != showPlay) {
             this.isShowPlay = showPlay;
-            setContentDescription(getContext().getString(showPlay ? R.string.play_label : R.string.pause_label));
-            if (isVideoScreen) {
-                setImageResource(showPlay ? R.drawable.ic_play_video_white : R.drawable.ic_pause_video_white);
-            } else if (!isShown()) {
-                setImageResource(showPlay ? R.drawable.ic_play_48dp : R.drawable.ic_pause);
-            } else if (showPlay) {
-                AnimatedVectorDrawableCompat drawable = AnimatedVectorDrawableCompat.create(
-                        getContext(), R.drawable.ic_animate_pause_play);
-                setImageDrawable(drawable);
-                drawable.start();
-            } else {
-                AnimatedVectorDrawableCompat drawable = AnimatedVectorDrawableCompat.create(
-                        getContext(), R.drawable.ic_animate_play_pause);
-                setImageDrawable(drawable);
-                drawable.start();
+            setContentDescription(getContext().getString(getLabel(showPlay)));
+            if (isVideoScreen || !isShown()) {
+                setImageResource(getImageResource(showPlay));
+                return;
             }
+            generateImageDrawable(showPlay);
         }
+    }
+
+    private int getLabel(boolean showPlay){
+        return showPlay ? R.string.play_label : R.string.pause_label;
+    }
+
+    private int getImageResource(boolean showPlay){
+        if(isVideoScreen){
+            return showPlay ? R.drawable.ic_play_video_white : R.drawable.ic_pause_video_white;
+        }
+        return showPlay ? R.drawable.ic_play_48dp : R.drawable.ic_pause;
+    }
+
+    private void generateImageDrawable(boolean showPlay){
+        AnimatedVectorDrawableCompat drawable = AnimatedVectorDrawableCompat.create(
+                getContext(), showPlay ? R.drawable.ic_animate_pause_play : R.drawable.ic_animate_play_pause);
+        setImageDrawable(drawable);
+        drawable.start();
     }
 }
